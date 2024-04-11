c7285f404aa148f046d183b572659d011ec8e82b
Pedro Manga <pedroblack1504@gmail.com>
2024-04-11 13:56:11 +0200

    Funcion playlistDowloader ahora utiliza for of en vez de for in para que las operaciones asincronas funcionen correctamente, he implementado con fs.access comprobar la existencia de los archivos antes de intentar descargarlos

--- utils/downloader.js
+++ utils/downloader.js
@@ -1,32 +1,41 @@
+//importamos los componentes necesarios
 const fs = require('fs');
 const https = require('https');
-const url = require('url');
 const axios = require('axios');
 
 //url y puerto del servidor
 const baseUrl = 'http://localhost:3000/';
 
-async function getFiles(ubicacion){
+//Esta función realiza una petición a nuestra api y nos devuelve un array 
+//con objetos que coinciden con los parámetros proporcionados
+async function getFiles(ubicacion) {
     const response = await axios.get(`${baseUrl}files?ubicacion=${ubicacion.toLowerCase()}`);
     return response.data
 }
 
 
 // esta función se encarga de mandar cada objeto 
-//recuperado a de la api para que su respectivo video sea descargado
+// recuperado a de la api para que su respectivo video sea descargado
 // usando la funcion fileDownloader
-async function playListDownloader(){
-    let url, extension, nombre
-    const datos=await getFiles('gandia')
-    datos.forEach(element => {
-        //console.log(element.datos.url)
-        url = element.datos.url
-        nombre = element.nombre
-        extension = element.datos.format
-        fileDowloader(extension,url,nombre)
-    });
-    console.log('descargas finalizadas')
+async function playListDownloader() {
+    const datos = await getFiles('gandia');
+    for (const element of datos) {
+        const url = element.datos.url;
+        const nombre = element.nombre;
+        const extension = element.datos.format;
+        const file = `./files/${nombre}.${extension}`;
+        //comprobamos que el archivo existe
+        try {
+            await fs.promises.access(file, fs.constants.F_OK);
+            console.log('El archivo existe');
+        } catch (err) {
+            console.error('El archivo no existe');
+            await fileDowloader(extension, url, nombre);
+        }
+    }
+    console.log('Descargas finalizadas');
 }
+
 
 //Esta función se encarga de descargar el archivo mediante la URL proporcionada, nombre y extesnsión
 //este archivo es guardado en la carpeta /files + nombre del archivo
