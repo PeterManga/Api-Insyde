a36b04a9fcd4fcfb9c29b332a455d9f5fa169384
Pedro Manga <pedroblack1504@gmail.com>
2024-05-16 13:13:52 +0200

    despliegue inicial de calendarPAge: se muestra el calendario donde podremos asignar eventos a cada player, en español y fechas formateadas

--- models/calendar.model.js
+++ models/calendar.model.js
@@ -1,22 +1,37 @@
 
- //importación de módulos
- const mongoose = require('mongoose');
+//importación de módulos
+const mongoose = require('mongoose');
 
- const calendarSchema = mongoose.Schema({
+const calendarSchema = mongoose.Schema({
     nombre: {
         type: String,
         required: true
     },
-    playlists: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'playlist.model'
-    }],
-    fechaInicio: {
-        type: Date,
-        required: true
-    }
-    
- },{
+    evento: [{
+        playlistId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'playlist.model'
+        },
+        playlistName: String,
+        playerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'player.model'
+        },
+        playerName: String,
+        fechaInicio: {
+            type: Date,
+            required: true
+        },
+        fechaFin: {
+            type: Date,
+            required: true
+        },
+        eventoName: String,
+        eventoDescripcion: String,
+        repetir: Boolean
+    }]
+
+}, {
     //Este campo añade la fecha de creación y la fecha de actualizacion
     timestamps: true
 })
