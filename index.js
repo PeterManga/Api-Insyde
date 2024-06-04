c015bc8f63a35a7a55d365befb2962c06a2d28ea
Pedro Manga <pedroblack1504@gmail.com>
2024-06-04 12:30:11 +0200

    versel deploy config

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-//importamos los modulos necesarios
 const mongoose = require('mongoose');
 const app = require('./app')
 require('dotenv').config();
 mongoose.Promise = global.Promise;
 
-//Obtenemos la url de la conexion
-const url =process.env.URL
+const url = process.env.URL;
 
-//realizamos la conexion con mongoose
-mongoose.connect(url);
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-
-//Definir el puerto donde escuchará la aplicacion
-const PORT = process.env.PORT;
-
-//iniciar el servidor en el puerto 3000
-app.listen(PORT, () => {
-    console.log(`Servidor Express en ejecucuón: Puerto ${PORT}`)
-})
-
+module.exports = app;
